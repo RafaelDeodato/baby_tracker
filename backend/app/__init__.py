8dc89b58584ab39c9cff101a19996cec053d6a64
Rafael Deodato <rafael.deodato.ext@vtal.com>
2026-06-09 18:18:35 -0300

    chore: configuração do SQLAlchemy e do Alembic

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from core.settings import Config
+from db.base import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
     JWTManager(app)
 
     @app.route("/health")
